056cae354a980cc9eb1d89c902932dc7ce4f7ba8
Johannes Zimmer <johannes@linity.one>
2024-11-17 23:43:30 +0100

    bugfix

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,37 +1,36 @@
-import {FC, forwardRef, HTMLAttributes} from 'react'
-import {cva,VariantProps} from "cva";
-import {cn} from "../../lib/cn";
+import { FC, forwardRef, HTMLAttributes } from "react";
+import { cva, VariantProps } from "cva";
+import { cn } from "../../lib/cn";
 
+const CardVariants = cva("group/card p-6 bg-white shadow-400 rounded-xl", {
+  variants: {
+    variant: {
+      static: "",
+      hover_shadow_increase: "transition-shadow duration-300 hover:shadow-600",
+      hover_liftoff:
+        "transition-all duration-300 hover:shadow-600 hover:-translate-y-0.25",
+    },
+  },
+  compoundVariants: [],
+  defaultVariants: {
+    variant: "static",
+  },
+});
 
-const CardVariants = cva(
-    'group/card mb-6 p-6 bg-white shadow-400 rounded-xl',
-    {
-        variants: {
-            variant: {
-                static: '',
-                hover_shadow_increase: 'transition-shadow duration-300 hover:shadow-600',
-                hover_liftoff:'transition-all duration-300 hover:shadow-600 translate-y-1',
-            }
-        },
-        compoundVariants: [
-        ],
-        defaultVariants: {
-            variant: 'static',
-        }
-    })
+interface CardProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof CardVariants> {}
 
-interface CardProps extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof CardVariants> {
-
-}
-
-const Card: FC<CardProps> = forwardRef<HTMLDivElement,CardProps>(
-    ({variant ,className,children,...props},ref) => {
-
-    return <div ref={ref} className={cn(CardVariants({variant}),className)}>
+const Card: FC<CardProps> = forwardRef<HTMLDivElement, CardProps>(
+  ({ variant, className, children, ...props }, ref) => {
+    return (
+      <div ref={ref} className={cn(CardVariants({ variant }), className)}>
         {children}
-    </div>
-})
+      </div>
+    );
+  },
+);
 
-Card.displayName = 'Card'
+Card.displayName = "Card";
 
-export default Card
+export default Card;

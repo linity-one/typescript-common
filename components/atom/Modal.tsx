e6d4c6720a6c5d38a261e12dab36f364ebd49b69
Johannes Zimmer <johannes@linity.one>
2024-12-29 23:17:23 +0100

    some changes + moved editor out

--- components/atom/Modal.tsx
+++ components/atom/Modal.tsx
@@ -1,21 +1,40 @@
 import { FC, ReactNode } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import { XIcon } from '../ui/Icons'
+import { CardVariants } from '../ui/Card'
+import { VariantProps } from 'cva'
+import { cn } from '../../lib/cn'
 
 interface ModalProps {
+    variant: VariantProps<typeof CardVariants>['variant']
     isOpen: boolean
     close: () => void
     className?: string
     children: ReactNode
     title?: string
+    backdrop: boolean
 }
 
-const Modal: FC<ModalProps> = ({ title, isOpen, close, className, children }) => {
+const Modal: FC<ModalProps> = ({
+    variant,
+    title,
+    isOpen,
+    close,
+    className,
+    children,
+    backdrop,
+}) => {
     return (
         <Dialog open={isOpen} onClose={() => close()}>
-            <DialogBackdrop className="fixed inset-0 bg-primary-950/60" />
+            {backdrop && <DialogBackdrop className="fixed inset-0 bg-primary-950/60" />}
             <div className="fixed flex inset-0 w-screen items-center justify-center p-8">
-                <DialogPanel className="flex flex-col gap-2 min-w-60 bg-white w-fit p-xl rounded-xl">
+                <DialogPanel
+                    className={cn(
+                        CardVariants({ variant, hover: 'static', flow: 'vertical' }),
+                        'min-w-60 ',
+                        className,
+                    )}
+                >
                     <div className="flex flex-row justify-between">
                         {title && <DialogTitle as="h3">{title}</DialogTitle>}
                         <div onClick={() => close()}>

88fa5df96a57db5bb4387b73386d46409d0c94e9
VivekSGopalakrishnan <gv8464@srmist.edu.in>
2025-02-13 03:47:02 +0530

    updated padding for card in smaller screen

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -2,30 +2,33 @@ import { FC, forwardRef, HTMLAttributes } from 'react'
 import { cva, VariantProps } from 'cva'
 import { cn } from '../../lib/cn'
 
-export const CardVariants = cva('group/card p-xl bg-white shadow-400  flex gap-1', {
-    variants: {
-        variant: {
-            standard: 'rounded-xl',
-            sharp: 'rounded-none',
+export const CardVariants = cva(
+    'group/card py-8 px-2 tablet:p-xl bg-white shadow-400  flex gap-1',
+    {
+        variants: {
+            variant: {
+                standard: 'rounded-xl',
+                sharp: 'rounded-none',
+            },
+            hover: {
+                static: '',
+                shadow_increase: 'transition-shadow duration-300 hover:shadow-600',
+                liftoff:
+                    'transition-all duration-300 hover:shadow-800 hover:-translate-y-0.25 hover:scale-[1.005]',
+            },
+            flow: {
+                vertical: 'flex-col',
+                horizontal: 'flex-row',
+            },
         },
-        hover: {
-            static: '',
-            shadow_increase: 'transition-shadow duration-300 hover:shadow-600',
-            liftoff:
-                'transition-all duration-300 hover:shadow-800 hover:-translate-y-0.25 hover:scale-[1.005]',
-        },
-        flow: {
-            vertical: 'flex-col',
-            horizontal: 'flex-row',
+        compoundVariants: [],
+        defaultVariants: {
+            variant: 'standard',
+            hover: 'static',
+            flow: 'vertical',
         },
     },
-    compoundVariants: [],
-    defaultVariants: {
-        variant: 'standard',
-        hover: 'static',
-        flow: 'vertical',
-    },
-})
+)
 
 interface CardProps extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof CardVariants> {}
 
